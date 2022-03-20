79ebf6be1e7877a16c9e126ccd595eb7ea1ab674
Nhok95 <enrique_1095@hotmail.es>
2022-03-20 22:57:28 +0100

    Added city-country extractor

--- dblp_data_extractor2.py
+++ dblp_data_extractor2.py
@@ -26,6 +26,26 @@ def random_keywords():
 
 	return '|'.join(samp)
 
+def getCities():
+	df_city = pd.read_csv(join(os.getcwd(), 'world-cities.csv'), sep=',', encoding='utf8')
+	#We get the 50 countries with higher HD Index.
+	df_hdi = pd.read_csv(join(os.getcwd(), 'hdi-countries.csv'), sep=',', encoding='utf8', nrows=50)
+	
+	
+	df_city = df_city.filter(['name','country'])
+	country_list = list(df_hdi['country'])
+
+	print(country_list)
+
+	# We get 3 random cities of every country (replace is True due to there are some countries with less than 3 
+	# and, in these cases, we want to get that cities even if we have to duplicate it)
+	df_sample = df_city.groupby('country').sample(n=3, replace=True, random_state=1)
+
+	# We remove duplicates created in the previous operation
+	df_sample.drop_duplicates(inplace=True) 
+
+	#We return only countries with high HDI (just to filter to a small subset)
+	return df_sample[df_sample.country.isin(country_list)] 
 
 def lorem_abstract_generator():
 	return lorem.paragraph()
@@ -34,8 +54,17 @@ def conferences():
 	df = pd.read_csv(join(pathClean, 'output_proceedings.csv'), sep=';', encoding='utf8', low_memory=False)
 	df2 = pd.read_csv(join(pathClean, 'output_inproceedings.csv'), sep=';', encoding='utf8', low_memory=False)
 
-	#df = df.filter([])
+	# 
+	df = df.filter(['booktitle', 'title', 'key', 'year'])
 
+	df_cities = getCities()
+	
+
+	# booktitle -> conference name
+	# author -> author array
+	# year -> to create the relationship and include it as a new instance of Year.
+
+	# We assume a edition is identified by a city 
 	df2 = df2.filter(['booktitle','author','key','year'])
 	df2['year'] = pd.to_numeric(df2['year'], errors='ignore')
 	df2['abstract'] = [lorem_abstract_generator()]*len(df2)
@@ -46,8 +75,8 @@ def conferences():
 	print(df.head())
 	print(df2.head())
 
-	df = df.dropna()
-	df2 = df2.dropna()
+	df = df.dropna(axis='index')
+	df2 = df2.dropna(axis='index')
 	print("NEW SHAPE:")
 	print(df.shape)
 	print(df2.shape)
@@ -59,4 +88,5 @@ def conferences():
 	df2.to_csv(join(pathOut,'conferences_extracted.csv'), sep=';', encoding='utf8', index=False)
 
 
-conferences()
+#conferences()
+getCities()
