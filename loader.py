66af722ad68b8c7e12b79dcebecfd522aaeccb3a
Carlos GD <40788436+Cgd15@users.noreply.github.com>
2022-03-20 16:42:49 +0100

    Add files via upload

--- loader.py
+++ loader.py
@@ -19,7 +19,7 @@ class Neo4JLoader:
                 LOAD CSV WITH HEADERS FROM 'file:///output_author_2.csv' AS row 
                 FIELDTERMINATOR ';'
                 WITH row WHERE row.ID IS NOT NULL 
-                MERGE (a:Author { authorID:row.ID, name:row.author })
+                MERGE (s:Scientist { ID:row.ID, name:row.author })
                 """)
         
         print("Authors inserted")
@@ -53,11 +53,11 @@ class Neo4JLoader:
                 WITH row WHERE row.key IS NOT NULL AND row.title IS NOT NULL
                 MERGE (p:Paper { paperID: row.key, title: row.title, abstract: row.abstract})
                 WITH row, p, SPLIT(row.author, '|') AS author 
-                MERGE (a:Author { name: author[0]})
-                MERGE (p)-[:AUTHOR]->(a) 
+                MERGE (s:Scientist { name: author[0]})
+                MERGE (p)-[:AUTHOR]->(s) 
                 WITH row, p, SPLIT(row.author, '|') AS author 
                 UNWIND RANGE(1,SIZE(author)-1) as i
-                MERGE (p)-[:COAUTHOR]->(a:Author { name: author[i]})
+                MERGE (p)-[:COAUTHOR]->(s:Scientist { name: author[i]})
                 """)
         print("Authors for articles inserted")
 
