29f0c79d0304e50b10df7e5ad133e80c72fbb7cb
Nhok95 <enrique_1095@hotmail.es>
2022-03-22 10:22:31 +0100

    Script cleanup

--- dblp_data_extractor.py
+++ dblp_data_extractor.py
@@ -9,6 +9,7 @@ import numpy as np
 import constant
 
 pathClean = join(os.getcwd(),'cleaned')
+pathData = join(os.getcwd(),'data')
 pathOut = join(os.getcwd(), 'extracted')
 
 try:
@@ -25,6 +26,24 @@ def random_keywords():
 
 	return '|'.join(samp)
 
+def getCities():
+	df_city = pd.read_csv(join(pathData, 'world-cities.csv'), sep=',', encoding='utf8')
+	#We get the 30 countries with higher HD Index.
+	df_hdi = pd.read_csv(join(pathData, 'hdi-countries.csv'), sep=',', encoding='utf8', nrows=30)
+	
+	df_city = df_city.filter(['name','country'])
+	country_list = list(df_hdi['country'])
+
+	# We get 3 random cities from each country (replace is True due to there are some countries with less than 3 
+	# and, in these cases, we want to get those cities even if we have to double them)
+	df_sample = df_city.groupby('country').sample(n=3, replace=True, random_state=1)
+
+	# We remove duplicates created in the previous operation
+	df_sample.drop_duplicates(inplace=True) 
+
+	# We return only high HDI countries (just to filter to a small subset)
+	df = df_sample[df_sample.country.isin(country_list)]
+	return  df.reset_index(drop=True)
 
 def lorem_generator():
 	return lorem.paragraph()
@@ -81,8 +100,61 @@ def add_random_reviewers_articles():
 	df_articles['decision'] = ['accepted']*len(df_articles)
 	df_articles.to_csv(join(pathOut,'journals_extracted.csv'), sep=';', encoding='utf8', index=False)
 
+def conferences():
+	df_cities = getCities() #83 cities
+	print(df_cities.shape)
+
+	df = pd.read_csv(join(pathClean, 'output_inproceedings.csv'), sep=';', encoding='utf8', low_memory=False)
+	df2 = pd.read_csv(join(pathClean, 'output_proceedings.csv'), sep=';', encoding='utf8', low_memory=False)
+	
+	# key -> article id in the system (not isbn or doi)
+	# title -> paper title
+	# author -> array of authors
+	# booktitle -> conference name
+	# year -> to create the relationship and include it as a new instance of Year.
+	df = df.filter(['key','title','author','booktitle','year'])
+	df['year'] = pd.to_numeric(df['year'], errors='ignore')
+	df['abstract'] = [lorem_generator()]*len(df)
+	df['keywords'] = [random_keywords() for i in range(0,len(df))]
+	print(df.shape)
+
+	# key -> article id in the system (not isbn or doi)
+	# title -> paper title
+	# booktitle -> conference name
+	# year -> to create the relationship and include it as a new instance of Year.
+	df2 = df2.filter(['key','title','booktitle','year'])
+	df2['year'] = pd.to_numeric(df2['year'], errors='ignore')
+
+	df = df.dropna(axis='index')
+	df2 = df2.dropna(axis='index')
+
+	# Edition and cities
+	rng = np.random.default_rng()
+	random_indexes = rng.integers(0, len(df_cities), size=len(df2))
+
+	cities = [[],[]]
+	for i in random_indexes:
+		cities[0].append(df_cities['name'].iloc[i])
+		cities[1].append(df_cities['country'].iloc[i])
+
+	df2['city'] = cities[0]
+	df2['country'] = cities[1]
+	# We assume a edition is identified by a the conference name, a city and a year
+	#insert edition
+	df2['edition'] = [str(x) for x in df2['year']]
+	df2['edition'] = df2['booktitle'] + "_" + df2['city'] + "_"+ df2['edition']
+
+	# Just for test get only the first 100 rows
+	df = df.head(constant.N_TEST)
+	df2 = df2.head(constant.N_TEST)
+
+	df.to_csv(join(pathOut,'conferences_extracted.csv'), sep=';', encoding='utf8', index=False)
+	df2.to_csv(join(pathOut,'conferences_extracted2.csv'), sep=';', encoding='utf8', index=False)
+
 
 
 journals_and_articles(constants.N_TEST)
 get_reviewers_random_pool()
 add_random_reviewers_articles()
+
+conferences()
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # !pip install neo4j
 import argparse
 from loader import Neo4JLoader
-from loader2 import Neo4JLoader2
 
 def GetParserArgs():
     parser = argparse.ArgumentParser()
@@ -25,19 +24,17 @@ if __name__ == "__main__":
 
     if args.password == None:
         loader = Neo4JLoader("bolt://localhost:7687")
-        loader2 = Neo4JLoader2("bolt://localhost:7687")
     else:
         loader = Neo4JLoader("bolt://localhost:7687", (args.user, args.password))
-        loader2 = Neo4JLoader2("bolt://localhost:7687", (args.user, args.password))
 
     if args.mode == 'load':
         print('Start bulk Loading...')
         loader.load_journals_articles()
         loader.load_authors_articles()
 
-        loader2.load_conference_articles()
-        loader2.load_authors_articles2()
-        loader2.load_conference_cities()
+        loader.load_conference_articles()
+        loader.load_authors_articles2()
+        loader.load_conference_cities()
         
         loader.load_article_reviews()
         loader.load_paper_citations()
