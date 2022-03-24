53112825068563b19e0f30abb4f924727cd578d8
Nhok95 <enrique_1095@hotmail.es>
2022-03-24 12:31:19 +0100

    constants Updated

--- constant.py
+++ constant.py
@@ -1,2 +1,9 @@
 N = 2000000
 N_TEST = 4000
+
+DATABASE_COMMUNITY_KEYWORDS = [
+    "Data Management", "Indexing", 
+    "Data Modeling", "Big Data", 
+    "Data Processing", "Data Storage", 
+    "Data Querying"
+]
